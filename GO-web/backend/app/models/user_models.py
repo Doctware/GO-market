6a3eb50c1cb56d95587d9997229df012fe4abd8a
Doctware <yusclever9@gmail.com>
2024-09-23 00:50:07 +0100

    v0.1 user model

--- GO-web/backend/app/models/user_models.py
+++ GO-web/backend/app/models/user_models.py
@@ -16,7 +16,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(512))
     role = db.Column(db.String(10), nullable=False)
     whatsapp = db.Column(db.String(20))
 
