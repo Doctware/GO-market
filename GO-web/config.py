af815e696ac6f781cb766f4c3a17a7acedf47515
Doctware <yusclever9@gmail.com>
2024-09-22 09:20:40 +0100

    configuration file

--- GO-web/config.py
+++ GO-web/config.py
@@ -1,9 +1,44 @@
 #!/usr/bin/python3
-""" this module contains configuraton """
+""" This module contains configuration class """
 import os
 
 
 class Config:
+    """ 
+    The class config:
+    Setting up secret key and database connection
+    Creating environment variable
+    """
     SECRET_KEY = os.getenv('SECRET_KEY', '52242')
-    SQLALCHEMY_DB_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://doctware:Adeyemi,52242./gomarket_db')
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL', 'mysql+pymysql://go_doctware:Adeyemi52242@localhost/gomarket_db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Enabling CORS for specific headers
+    CORS_HEADERS = 'Content-Type'
+
+    # Optionally enable debug mode
+    DEBUG = os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1']
+
+
+class DevelopmentConfig(Config):
+    """ Development configuration class
+        Setting database connection for development phase
+    """
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DB_URL', 'mysql+pymysql://go_doctware:Adeyemi52242@localhost/gomarket_db')
+
+
+class ProductionConfig(Config):
+    """ Production Configuration class
+        Setting up database connection for production phase
+    """
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL', 'mysql+pymysql://go_doctware:Adeyemi52242@loalhost/gomarket_db')
+
+
+# Manage environment variables
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
